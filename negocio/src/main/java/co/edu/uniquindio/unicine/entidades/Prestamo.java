fea886211c46fdb5e8ea49d9fed01dba215ae70e
Juanj-O <juan.osorio@celuweb.com>
2022-09-28 12:06:17 -0500

    Mapeo de tablas con entidades

---
+++ negocio/src/main/java/co/edu/uniquindio/unicine/entidades/Prestamo.java
@@ -0,0 +1,28 @@
+package co.edu.uniquindio.unicine.entidades;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString
+public class Prestamo implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Integer codigo;
+
+    @Column(nullable = false)
+    private LocalDateTime fechaPrestamo;
+    private LocalDate fechaDevolucion;
+}
