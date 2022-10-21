5184fc36bd5345db0bb708f8c8c30aeca4fff142
julianaarias12 <julianaarias123000@gmail.com>
2022-10-21 15:00:35 -0500

    repo

--- negocio/src/main/java/co/edu/uniquindio/unicine/repo/AdministradorRepo.java
+++ negocio/src/main/java/co/edu/uniquindio/unicine/repo/AdministradorRepo.java
@@ -1,12 +1,10 @@
 package co.edu.uniquindio.unicine.repo;
 
-import co.edu.uniquindio.unicine.entidades.Cliente;
+import co.edu.uniquindio.unicine.entidades.Administrador;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClienteRepo extends JpaRepository<Cliente, String> {
-
-
-
+public interface AdministradorRepo extends JpaRepository<Administrador, String> {
 }
--- negocio/src/main/java/co/edu/uniquindio/unicine/repo/CuponRepo.java
+++ negocio/src/main/java/co/edu/uniquindio/unicine/repo/CuponRepo.java
@@ -1,12 +1,9 @@
 package co.edu.uniquindio.unicine.repo;
 
-import co.edu.uniquindio.unicine.entidades.Cliente;
+import co.edu.uniquindio.unicine.entidades.Cupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClienteRepo extends JpaRepository<Cliente, String> {
-
-
-
+public interface CuponRepo extends JpaRepository<Cupon, Integer> {
 }
