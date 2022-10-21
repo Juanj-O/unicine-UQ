1bca8f8e2214a530a5bc837bca44f418f8ba3f64
JuanJ-O <juanj.osoriog1@uqvirtual.edu.co>
2022-10-21 15:11:27 -0500

    cambios

--- negocio/src/main/java/co/edu/uniquindio/unicine/repo/AdminRepo.java
+++ negocio/src/main/java/co/edu/uniquindio/unicine/repo/AdminRepo.java
@@ -3,5 +3,5 @@ package co.edu.uniquindio.unicine.repo;
 import co.edu.uniquindio.unicine.entidades.Administrador;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AdminRepo extends JpaRepository<Administrador, Integer> {
+public interface AdminRepo extends JpaRepository<Administrador, String> {
 }
--- negocio/src/main/java/co/edu/uniquindio/unicine/repo/AdminTeatroRepo.java
+++ negocio/src/main/java/co/edu/uniquindio/unicine/repo/AdminTeatroRepo.java
@@ -3,5 +3,5 @@ package co.edu.uniquindio.unicine.repo;
 import co.edu.uniquindio.unicine.entidades.AdministradorTeatro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AdminTeatroRepo extends JpaRepository<AdministradorTeatro, Integer> {
+public interface AdminTeatroRepo extends JpaRepository<AdministradorTeatro, String> {
 }
