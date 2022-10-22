e32d440560fea3bae5133d9be4de8c5e203bd820
JuanJ-O <juanj.osoriog1@uqvirtual.edu.co>
2022-10-22 13:57:31 -0500

    Servicios

--- negocio/src/main/java/co/edu/uniquindio/unicine/servicios/ClienteServicio.java
+++ negocio/src/main/java/co/edu/uniquindio/unicine/servicios/ClienteServicio.java
@@ -2,24 +2,28 @@ package co.edu.uniquindio.unicine.servicios;
 
 import co.edu.uniquindio.unicine.entidades.Cliente;
 import co.edu.uniquindio.unicine.entidades.Compra;
+import co.edu.uniquindio.unicine.entidades.Pelicula;
 
 import java.util.List;
 
 public interface ClienteServicio {
 
-    Cliente login(String correo, String password);
+    Cliente login(String correo, String password) throws Exception;
 
-    Cliente registrarCliente(Cliente cliente);
+    Cliente registrarCliente(Cliente cliente) throws Exception;
 
-    Cliente actualizarCliente(Cliente cliente);
+    Cliente actualizarCliente(Cliente cliente) throws Exception;
 
-    void eliminarCliente(Integer codigoCliente);
+    void eliminarCliente(String cedulaCliente) throws Exception;
 
     List<Cliente> listarCliente();
 
-    List<Compra> listarHistorial(Integer codigoUsuario);
+    List<Compra> listarHistorial(String cedulaCliente) throws Exception;
 
-    Compra hacerCompra(Compra compra);
+    Compra hacerCompra(Compra compra) throws Exception;
 
-    boolean redimirCupon(Integer codigoCuenta);
+    boolean redimirCupon(Integer codigoCuenta) throws Exception;
+
+    List<Pelicula> buscarPeliculas(String nombrePelicula) throws Exception;
+
 }
