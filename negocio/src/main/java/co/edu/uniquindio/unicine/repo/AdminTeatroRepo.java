23ce4a6a44add25780114be5b1c246e9c3afdd64
JuanJ-O <juanj.osoriog1@uqvirtual.edu.co>
2022-10-22 09:46:26 -0500

    eliminar dos repo

--- negocio/src/main/java/co/edu/uniquindio/unicine/repo/AdminTeatroRepo.java
+++
@@ -1,7 +0,0 @@
-package co.edu.uniquindio.unicine.repo;
-
-import co.edu.uniquindio.unicine.entidades.AdministradorTeatro;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminTeatroRepo extends JpaRepository<AdministradorTeatro, Integer> {
-}
